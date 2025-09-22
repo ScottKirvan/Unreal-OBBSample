f0aa61e1483ce2546385625ae179b081e24d0e93
scott kirvan <fork@skvfx.com>
2025-09-22 14:46:20 -0700

    Refactor: move keystore generation to it's own script

--- setup.bat
+++ setup.bat
@@ -27,20 +27,9 @@ call "%~dp0config.bat" || (
     EXIT /B 1
 )
 
-if not exist "Build\Android\" (
-    mkdir "Build\Android"
+call "%~dp0scripts\create_keystore.bat" || (
+    EXIT /B 1
 )
-
-REM DO NOT overwrite an existing keystore, as it is needed for signing updates
-REM create the keystore one time and reuse it.  Once you delete the keystore, you
-cannot upload an updated version to the same app on the app store
-REM TODO - make the setup script interactive so users can choose to overwrite or create new if needed
-REM TODO - same goes for the section that generates the big bmp assets below
-REM REM if exist "Build\Android\%MY_KEYSTORE%" (
-REM REM     del "Build\Android\%MY_KEYSTORE%"
-REM REM )
-
-REM REM "%KEYTOOL_PATH%" -genkey -v -keystore Build\Android\%MY_KEYSTORE% -alias %MY_KEYSTORE_ALIAS% -keyalg RSA -keysize 2048 -validity 10000 -dname "%MY_KEYSTORE_DNAME%" -storepass %MY_KEYSTORE_PASSWORD% -keypass %MY_KEYSTORE_PASSWORD%
 
 if not exist "%DEFAULT_ENGINE_INI_FILE%" (
     echo Could not find DefaultEngine.ini.
