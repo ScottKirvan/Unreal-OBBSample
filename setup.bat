5f9af68b28e62618fb24c28cae8ab22381a6c8f7
scott kirvan <fork@skvfx.com>
2025-09-22 11:42:54 -0700

    removed keystore generation, hardcoded variable, and redundant validation
     - remove keystore creation from setup - deleting and rebuilding keystore will break the app's cert
     - added the keystore filename to the unreal default.ini updates
     - removed a redundant check for the KEYTOOL_PATH

--- setup.bat
+++ setup.bat
@@ -2,12 +2,14 @@
 REM ===============================================================
 REM Copyright (c) Meta Platforms, Inc. and affiliates.
 REM All rights reserved.
+REM Modifications Copyright (c) 2025, Scott Kirvan, Aemulus-XR
 REM
 REM This source code is licensed under the MIT license found in the
 REM LICENSE file in the root directory of this source tree.
 REM ===============================================================
 
 REM ============================================================
+REM file:: setup.bat
 REM This batch script configures the project environment and
 REM performs several tasks necessary for building the project.
 REM
@@ -25,20 +27,20 @@ call "%~dp0config.bat" || (
     EXIT /B 1
 )
 
-if not exist "%KEYTOOL_PATH%" (
-    echo "android's keytool.exe not found at %KEYTOOL_PATH%"
-    EXIT /B 1
-)
-
 if not exist "Build\Android\" (
     mkdir "Build\Android"
 )
 
-if exist "Build\Android\ExampleKey.keystore" (
-    del "Build\Android\ExampleKey.keystore"
-)
+REM DO NOT overwrite an existing keystore, as it is needed for signing updates
+REM create the keystore one time and reuse it.  Once you delete the keystore, you
+cannot upload an updated version to the same app on the app store
+REM TODO - make the setup script interactive so users can choose to overwrite or create new if needed
+REM TODO - same goes for the section that generates the big bmp assets below
+REM REM if exist "Build\Android\%MY_KEYSTORE%" (
+REM REM     del "Build\Android\%MY_KEYSTORE%"
+REM REM )
 
-"%KEYTOOL_PATH%" -genkey -v -keystore Build\Android\ExampleKey.keystore -alias %MY_KEYSTORE_ALIAS% -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=John Doe, OU=Engineering, O=MyCompany, L=MyCity, ST=MyState, C=US" -storepass %MY_KEYSTORE_PASSWORD% -keypass %MY_KEYSTORE_PASSWORD%
+REM REM "%KEYTOOL_PATH%" -genkey -v -keystore Build\Android\%MY_KEYSTORE% -alias %MY_KEYSTORE_ALIAS% -keyalg RSA -keysize 2048 -validity 10000 -dname "%MY_KEYSTORE_DNAME%" -storepass %MY_KEYSTORE_PASSWORD% -keypass %MY_KEYSTORE_PASSWORD%
 
 REM check if MY_PACKAGE_NAME is defined
 if "%MY_PACKAGE_NAME%"=="" (
@@ -55,6 +57,10 @@ powershell -File scripts\Set-IniVal.ps1 -IniKey PackageName -IniVal %MY_PACKAGE_
     echo "Failed to set PackageName in DefaultEngine.ini"
     EXIT /B 1
 )
+powershell -File scripts\Set-IniVal.ps1 -IniKey KeyStore -IniVal %MY_KEYSTORE% -IniPath %DEFAULT_ENGINE_INI_FILE% || (
+    echo "Failed to set KeyStore in DefaultEngine.ini"
+    EXIT /B 1
+)
 powershell -File scripts\Set-IniVal.ps1 -IniKey KeyAlias -IniVal %MY_KEYSTORE_ALIAS% -IniPath %DEFAULT_ENGINE_INI_FILE% || (
     echo "Failed to set KeyAlias in DefaultEngine.ini"
     EXIT /B 1
@@ -64,7 +70,7 @@ powershell -File scripts\Set-IniVal.ps1 -IniKey KeyStorePassword -IniVal %MY_KEY
     EXIT /B 1
 )
 
-if not exist %UNREALEDITOR_CMD_EXE% (
+if not exist "%UNREALEDITOR_CMD_EXE%" (
     echo "UnrealEditor-Cmd.exe not found at %UNREALEDITOR_CMD_EXE%"
     EXIT /B 1
 )
